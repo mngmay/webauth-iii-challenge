4c7c2eae278e2044afc21e614773b7a6a52fe90d
May Ng <mngmay@gmail.com>
2019-09-18 12:41:46 -0700

    Created useersModel helper functions

---
+++ api/auth/authRouter.js
@@ -0,0 +1,5 @@
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const Users = require("../users/usersModel.js");
---
+++ api/users/usersModel.js
@@ -0,0 +1,30 @@
+const db = require("../data/dbConfig.js");
+
+module.exports = {
+  add,
+  find,
+  findById,
+  findBy
+};
+
+function find() {
+  return db("users");
+}
+
+function findById(id) {
+  return db("users")
+    .where({ id })
+    .first();
+}
+
+function findBy(filter) {
+  return db("users").where(filter);
+}
+
+// async because we are both inserting to the table, and calling a query to return user object.
+async function add(user) {
+  // destructure id from user object to use in findById query
+  const [id] = await db("users").insert(user);
+
+  return findById(id);
+}
