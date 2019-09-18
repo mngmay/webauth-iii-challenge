7c484ac003fc3e2cff627e7c3fb3eb28f6856905
May Ng <mngmay@gmail.com>
2019-09-18 13:04:27 -0700

    posting new user works

--- api/auth/authRouter.js
+++ api/auth/authRouter.js
@@ -3,3 +3,19 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 const Users = require("../users/usersModel.js");
+
+router.post("/register", (req, res) => {
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
